2305e1b9839609c2a4080fcc62db0452f2b11e16
Yogesh Sathe <ysathe991@gmail.com>
2023-12-18 19:03:56 +0530

    portfolio updates

--- main.js
+++ main.js
@@ -10,19 +10,3 @@ navClose.addEventListener('click', () => {
    navMenu.classList.remove('show-menu')
 })
 
-
-var app = document.getElementById('app');
-
-var typewriter = new Typewriter(app, {
-   loop: true
-});
-
-typewriter.typeString('Web Developer')
-   .pauseFor(2500)
-   .deleteAll()
-   .typeString('Web Designer')
-   .pauseFor(2500)
-   .deleteAll()
-   .typeString('<strong>FrontEnd Developer</strong>')
-   .pauseFor(2500)
-   .start();
