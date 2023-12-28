7699f2ca64a9f4d1a8037bdbe2abe1f479b9d952
Yogesh Sathe <ysathe991@gmail.com>
2023-12-28 16:08:21 +0530

    updating

---
+++ script.js
@@ -0,0 +1,18 @@
+//Javascript for Navigation effect on scroll
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+//Javascript for responsive navigation sidebar Nav
+var menu = document.querySelector('.menu');
+var menuBtn = document.querySelector('.menu-btn1');
+var closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.add('active');
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove('active');
+});
